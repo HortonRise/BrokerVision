65104e7da3fe7e5873a6fefb4835eb56d4837361
Jonathan Larson <jonathan.larson@riseinteractive.com>
2016-08-27 23:16:56 -0500

    small updates/cleanup

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,40 +1,14 @@
 $( document ).ready(function() {
-    //console.log(test);
 
-    // updateGraph(valuesArray1, 1);
-    // updateGraph(valuesArray2, 2);
 
     setTimeout(function(){
         updatePage();
-    }, 1000);
+    }, 2000);
 
-
-    // setTimeout(function(){
-    //     updateGraph(valuesArray1, 2);
-    //     updateGraph(valuesArray2, 1);
-    //
-    // }, 2000);
-    //
-    // setTimeout(function(){
-    //     updateBars(barArray);
-    //     updateFacts(barArray);
-    // }, 2000);
-
-    //
-    //top lines
-    //
-    // setTimeout(function(){
-    //     adjustLines();
-    // }, 1000);
-
-
-    //
-    //clock
-    //
     setTimeout(function(){
         $('.ring1').css('opacity', '1');
-        moveClock(6000);
-        displayClock(6000);
+        moveClock(60000);
+        displayClock(60000);
     }, 500);
 
 
@@ -57,17 +31,12 @@ function getData() {
 
        console.log(dataObj);
     });
+    setTimeout(function(){
+        getData();
+    }, 2000);
 }
 
 getData();
-
-
-
-
-//graph value max 60 currently
-//preferences max 40
-//100 total val
-
 
 
 function moveClock(time){
@@ -140,31 +109,11 @@ function tickDown(minutes, seconds){
 
     }else{
         //do nothing
+        $('.clockNumbers').css('color','red');
     }
 }
 
 //////END CLOCK
-
-
-
-
-// var valuesArray1 = [];
-// var value1 = {value:"$100", perc:"100%", pref:".9"};
-// var value2 = {value:"$60", perc:"60%", pref:".2"};
-// var value3 = {value:"$80", perc:"80%",  pref:".4"};
-// valuesArray1.push(value1);
-// valuesArray1.push(value2);
-// valuesArray1.push(value3);
-//
-//
-// var valuesArray2 = [];
-// var value1b = {value:"$60", perc:"60%", pref:".4"};
-// var value2b = {value:"$80", perc:"80%", pref:".9"};
-// var value3b = {value:"$100", perc:"100%", pref:".2"};
-// valuesArray2.push(value1b);
-// valuesArray2.push(value2b);
-// valuesArray2.push(value3b);
-
 
 
 
@@ -184,63 +133,33 @@ function updateGraph(graphNum){
             currentObj = dataObj.term;
         }
 
-    console.log(currentObj);
+    //console.log(currentObj);
 
     for(var j=0; j<3;j++){
 
-        var varClass = ".var" + graphNum;
+        var varClass = ".var" + (graphNum+1);
         var barFullClass = varClass + ' .graph' + ' .barsWrapper' + ' .bar' + (j+1);
         var valFullClass = varClass + ' .graph' + ' .barsWrapper' + ' .bar' + (j+1) + ' .barVal' + (j+1);
         var prefFullClass = varClass + ' .graph' + ' .pref';
         var prefFullValClass = varClass + ' .graph' + ' .pref .prefVal';
         var transformedVal = currentObj[j].percent;
-        console.log(transformedVal);
+        //console.log(transformedVal);
+
+
 
         $(barFullClass).css('height', transformedVal);
         $(valFullClass).html(currentObj[j].value);
-        //$(prefFullClass).css('opacity', currentObj[j].pref);
-        //$(prefFullValClass).html(transformedVal);
+
 
     };
+
+    var prefWeight = currentObj[0].weight;
+    $(prefFullClass).css('opacity', prefWeight);
+    $(prefFullValClass).html((prefWeight*100) + "%");
 }
 
-// function updateGraph(graphVals, graphNum){
-//     for(var j=0; j<3;j++){
-//
-//         var varClass = ".var" + graphNum;
-//         var barFullClass = varClass + ' .graph' + ' .barsWrapper' + ' .bar' + (j+1);
-//         var valFullClass = varClass + ' .graph' + ' .barsWrapper' + ' .bar' + (j+1) + ' .barVal' + (j+1);
-//         var prefFullClass = varClass + ' .graph' + ' .pref';
-//         var prefFullValClass = varClass + ' .graph' + ' .pref .prefVal';
-//         var transformedVal = (graphVals[j].pref*100) + "%";
-//
-//         $(barFullClass).css('height', graphVals[j].perc);
-//         $(valFullClass).html(graphVals[j].value);
-//         $(prefFullClass).css('opacity', graphVals[j].pref);
-//         $(prefFullValClass).html(transformedVal);
-//
-//     };
-// }
 
 //TOP SUMMARY AND FACTS
-
-// var barArray = [];
-// var value1c = {value:9.0, cost:'$2.2m'};
-// var value2c = {value:5.2, cost:'$2.8m'};
-// var value3c = {value:7.1, cost:'$1.9m'};
-// barArray.push(value1c);
-// barArray.push(value2c);
-// barArray.push(value3c);
-
-// var barArray = [];
-// var value1c = {value:0.0, cost:'$0.0m'};
-// var value2c = {value:0.0, cost:'$0.0m'};
-// var value3c = {value:0.0, cost:'$0.0m'};
-// barArray.push(value1c);
-// barArray.push(value2c);
-// barArray.push(value3c);
-
-
 
 function updateBars(){
     for(var k=0; k<3;k++){
@@ -264,33 +183,6 @@ function updateBars(){
         $(propValWrapId).css('left', barWidth);
     }
 }
-// function updateBars(barVals){
-//     for(var k=0; k<3;k++){
-//
-//         var barWidth = (barVals[k].value*10) + "%";
-//
-//         var propId = ".propertyLine" + (k+1);
-//         var propImgWrapId = ".propertyImgWrapper" + (k+1);
-//         var propValWrapId = ".propertyValueWrapper" + (k+1);
-//
-//
-//         $(propId).css('width', barWidth);
-//         $(propImgWrapId).css('left', barWidth);
-//         $(propValWrapId).css('left', barWidth);
-//     }
-// }
-// function updateFacts(barVals){
-//     for(var l=0; l<3;l++){
-//
-//         var propFactId = ".propStat" + (l+1);
-//         var propFactNumId = propFactId + " .propFact1 .factNum";
-//         var propFact2NumId = propFactId + " .propFact2 .factNum";
-//
-//         $(propFactNumId).html(barVals[l].value);
-//         $(propFact2NumId).html(barVals[l].cost);
-//
-//     }
-// }
 
 
 function updateFacts(){
@@ -310,11 +202,13 @@ function updatePage(){
 
     updateBars();
     updateFacts();
-    console.log(dataObj.price);
+
     for(var m=0; m<5; m++){
-        console.log(m);
         updateGraph(m);
     }
 
+    setTimeout(function(){
+        updatePage();
+    }, 2000);
 
 }
